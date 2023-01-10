8721c8ebf5ff4f7cb7ed2258e40f0ae8b7027729
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2023-01-09 22:33:56 -0600

    mod

---
+++ 03_aritmethic.py
@@ -0,0 +1,16 @@
+# (+) Suma 
+print(5+5)
+print("hi "+" how r u?")
+# (-) Resta
+print(10-5)
+# (*) Multiplicación
+print(5*2)
+print("hi "*3)
+# (/) División
+print(10/2)
+# (%) Modulo o Residuo
+print(10%2)
+# (//) Division con valor entero
+print(15//2)
+# (**) Exponenciación
+print(5**3)
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-age=input("how old are you? => ")
-age=int(age)
-print(f"on 10 years you will be {age+10} years old")
