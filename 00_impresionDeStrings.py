0b01bbff5e77026f941f9f07573aa54ce4305494
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2023-01-30 22:57:32 -0600

    add cases to 00_impresionDeStrings

--- 00_impresionDeStrings.py
+++ 00_impresionDeStrings.py
@@ -2,13 +2,19 @@ name = "Rodrigo"
 last_name= "Flores"
 age = 22
 
-template="Hola "+ name + last_name +" , tu edad es "+ age
+#Creas un string y luego lo mandas a imprimir
+template="Hola "+ name + last_name +" , tu edad es "+ age 
 print('v1 ', template)
 
+#creas un string y con la función format introduces los valores en las llaves {}
 template="Hola mi nombre es {} y mi apellido es {}".format(name,last_name)
 print('v2',template)
 
+#imprimir todo concatenando strings y datos
 print("v3 Hola "+ name + last_name +" , tu edad es "+ age)
 
+#usar la función format como f al inicio del string como si fuera un parámetro de template
 template = f"Hola {name} {last_name}, tu edad es {age}"
 print('v4',template)
+
+print(f"V5 Hola {name} {last_name}, tu edad es {age}")
